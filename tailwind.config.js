2f6a861dc20621fd1a218689efbfd00e97479ef6
ilhamdwikiarifani <ilhamdwiki0408@gmail.com>
2022-03-04 18:21:19 +0700

    porto--ilham

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: "'Poppins', sans-serif",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-focus"],
+      opacity: ["group-focus"],
+      inset: ["group-focus"],
+    },
+  },
+  plugins: [],
+};
